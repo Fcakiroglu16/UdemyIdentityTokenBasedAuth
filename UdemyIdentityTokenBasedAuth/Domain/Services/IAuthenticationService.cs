3d7a465eb8db13350a3132ad89327a1ce68625ff
Fatih Çakıroğlu <f-cakiroglu@outlook.com>
2019-07-29 02:29:04 +0300

    Ders 8

---
+++ UdemyIdentityTokenBasedAuth/Domain/Services/IAuthenticationService.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.Twitter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UdemyIdentityTokenBasedAuth.Domain.Responses;
+using UdemyIdentityTokenBasedAuth.ResourceViewModel;
+
+namespace UdemyIdentityTokenBasedAuth.Domain.Services
+{
+    interface IAuthenticationService
+    {
+
+        Task<BaseResponse<UserViewModelResource>> SignUp(UserViewModelResource userViewModel);
+
+        Task<BaseResponse<AccessToken>> SignIn(SignInViewModelResource signInViewModel);
+
+        Task<BaseResponse<AccessToken>> CreateAccessTokenByRefreshToken(RefreshTokenViewModelResource refreshTokenViewModel);
+
+        Task<BaseResponse<AccessToken>> RevokeRefreshToken(RefreshTokenViewModelResource refreshTokenViewModel);
+
+
+
+
+    }
+}
